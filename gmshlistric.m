1636270047612ae4a7f0f7526f9ef6c4469f8e37
Jack Loveless <jloveles@smith.edu>
2019-07-22 12:38:37 -0400

    Modified gmshlistric.m

--- gmshlistric.m
+++ gmshlistric.m
@@ -10,30 +10,37 @@ function p = gmshlistric(edgex, edgey, edgez, top, bot, ls1, ls2, fs1, fs2, sz)
 %   
 
 np = length(edgex); % Number of points
+nrows = size(ls1, 2)/2; % Number of listric rows
 alledge = [top(:); ls1(:); fs1(:); bot(:); fs2(:); ls2(:)]; % All edges
-edgeend = cumsum([length(top), length(ls1), length(fs1), length(bot), length(fs2), length(ls2)]); % Edge indices
-edgebeg = [1 edgeend(1:end-1)+1]; 
+nedge = size(alledge, 1)/2; % Number of edges
 
 % Write coordinates to Gmsh .geo file
 %fn = sprintf('junk%g', rem(now, 1));
 fn = 'junk';
 fid = fopen([fn '.geo'], 'w');
-fprintf(fid, 'charl = %g;\n', sz); % Write element size
+charl = sz*ones(1, np); % Create vector of element size
+charl(bot) = 10*sz; % Make nodes along bottom have a larger size
 % Write points: top then bottom
-fprintf(fid, 'Point(%g) = {%g, %g, %g, charl};\n', [1:np; edgex(:)'; edgey(:)'; edgez(:)']);
+fprintf(fid, 'Point(%g) = {%g, %g, %g, %g};\n', [1:np; edgex(:)'; edgey(:)'; edgez(:)'; charl]);
 % Write lines: top, listric side 1, flat side 1, bottom, flat side 2, listric side 2
-for i = 1:6,
-   edge = alledge(edgebeg(i):edgeend(i));
-   fprintf(fid, 'BSpline(%g) = {%g', i, edge(1));
-   fprintf(fid, ', %g', edge(2:end));
-   fprintf(fid, '};\n');
+for i = 1:nedge
+   fprintf(fid, 'Line(%g) = {%g, %g};\n', i, alledge(2*i-1), alledge(2*i));
 end
-% Extra line that divides plane from curved surface
-fprintf(fid, 'Line(7) = {%g, %g};\n', fs1(1), fs2(2));
 
-% Circulate lines and create plane
-fprintf(fid, 'Line Loop(1) = {1, 2, 7, 6};\nSurface(1) = {1};\n');
-fprintf(fid, 'Line Loop(2) = {3, 4, 5, -7};\nSurface(2) = {2};\n');
+% Extra lines that separate panels
+for i = 1:nrows
+   fprintf(fid, 'Line(%g) = {%g, %g};\n', nedge+i, ls1(length(ls1)-2*(i-1)), ls2(2*i-1));
+end
+
+% Circulate lines and create surfaces
+fprintf(fid, 'Line Loop(1) = {%g, %g, %g, %g};\nSurface(1) = {1};\n', [nrows+(2:4), -(2*nrows+5)]); % Flat part of fault
+for i = 1:nrows % Listric panels
+   if i == 1
+      fprintf(fid, 'Line Loop(%g) = {%g, %g, %g, %g};\nSurface(%g) = {%g};\n', 1+i, 2, nedge+nrows, nedge, 1, 1+i, 1+i);
+   else 
+      fprintf(fid, 'Line Loop(%g) = {%g, %g, %g, %g};\nSurface(%g) = {%g};\n', 1+i, nrows+3-i, 2*nrows+i+3, nrows+i+3, -(2*nrows+i+4), 1+i, 1+i);
+   end
+end
 fclose(fid);
 
 % Mesh using Gmsh
