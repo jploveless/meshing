08b59df5c0014170352db8f37a84164fa55fec29
Jack Loveless <jloveles@smith.edu>
2021-05-28 14:27:04 -0400

    New basic meshing workflow added

---
+++ extractcoords.m
@@ -0,0 +1,75 @@
+function [c, nc] = extractcoords(file, depth)
+% extractcoords   Extracts and organizes coordinates from an ArcGIS text file.
+%   p = extractcoords(FILE, DEPTH) extract coordinates for the top of faults,
+%   and the bottom (with uniform z coordinate specified by input arugment DEPTH), 
+%   based on a FILE containing surface trace coordinates exported from ArcGIS.
+%   Individual objects are assumed to be identified in one of two ArcGIS file
+%   formats: 
+%   Each object begins with an FID line and ends with an END line
+%   Each row contains the x and y coordinates and the FID
+%
+%   The coordinates are returned to the 3-column array c, containing x, y, and z
+%   coordinates for the top and bottom. The number of coordinates corresponding 
+%   to each distinct object. 
+%
+%   Use the function rotatefaultcoords to assign dips to each fault (by changing the
+%   bottom coordinates) and function gmshfaults to create triangulated meshes of all
+%   faults.
+%
+
+% ArcGIS file style 1: Feature coordinates start with FID and end with "END"
+a = opentxt(file);
+ends = strfind(a(:, 1)', 'E') - 1;
+if ~isempty(ends)
+   astyle = 1;
+   if diff(ends(end), ends(end-1)) == 1
+      ends = ends(1:end-1);
+   end
+   begs = [2 ends(1:end-1) + 3];
+else
+   delims = strfind(a(:, 1)', '>');
+   if isempty(delims)
+      % ArcGIS file style 2: Each line contains feature x, y, FID
+      astyle = 2;
+      if a(1) == 'X'
+         fl = 2;
+      else
+         fl = 1;
+      end
+      a = str2num(a(fl:end, :));
+      d3 = diff(a(:, 3));
+      ends = [find(d3==1); size(a, 1)];
+      begs = [1; ends(1:end-1)+1];
+   else
+      % GMT style delimiters
+      astyle = 1;
+      begs = delims + 1;
+      ends = [delims(2:end) - 1, size(a, 1)];
+   end
+end
+
+% Number of coordinates in each object
+nc = 2*(ends+1-begs);
+% Blank coordinate array
+c = zeros(sum(nc), 3);
+
+% Initial coordinate indices
+idxt = [1-nc(1), -nc(1)/2];
+idxb = [1-nc(1)/2, 0];
+
+% Loop through objects
+for i = 1:numel(begs)
+   % Update indices
+   idxt = idxb(2) + [1, nc(i)/2];
+   idxb = idxb(2) + [nc(i)/2+1, nc(i)];
+   if astyle == 1
+      tc = str2num(a(begs(i):ends(i), :));
+      tc = tc(:, 1:2);
+   else
+      tc = a(begs(i):ends(i), 1:2);
+   end
+%   tc = unique(tc, 'rows', 'stable');
+   % Place coordinates 
+   c(idxt(1):idxb(2), 1:2) = [tc; flipud(tc)];
+   c(idxb(1):idxb(2), 3) = -abs(depth);
+end
---
+++ gmshcoords.m
@@ -0,0 +1,118 @@
+function p = gmshcoords(c, nc, sz, smoothtrace)
+% gmshfaults  Meshes faults in 3D using Gmsh.
+%   gmshfaults(TOPDIR, BOTDIR, ELSIZE) meshes all faults whose top coordinates
+%   are contained in text files within the folder TOPDIR and bottom coordinates
+%   are contained in text files within the folder BOTDIR, using the 
+%   element size specified using ELSIZE.  Geometry (.geo) and mesh (.msh)
+%   files are saved to the bottom coordinate directory.
+%
+%   gmshfaults(TOPDIR, BOTDIR, ELSIZE, SMOOTHTRACE) 
+%
+%   ALL = gmshfaults(...) returns a string to the command line that can be
+%   used to read all faults in using ReadPatches.m, i.e.
+%   >> p = ReadPatches(ALL);
+% 
+%
+
+if ~exist('smoothtrace', 'var')
+   smoothtrace = true;
+end
+if ~islogical(smoothtrace)
+   smoothtrace = logical(smoothtrace);
+end
+
+% Count number of entities
+nf = numel(nc);
+
+% Indices of each entity's beginning, midpoint, and end
+ends = cumsum(nc(:));
+begs = [1; ends(1:end-1)+1];
+mids = [nc(1)/2 + 1; ends(1:end-1) + nc(2:end)'/2 + 1];
+% Labels for 4 edges for each entity
+edgs = reshape(1:4*nf, 4, nf);
+
+% Create file
+fid = fopen('faults.geo', 'w');
+fprintf(fid, 'charl = %g;\n', sz);
+
+for i = 1:nf
+   fprintf(fid, 'Point(%g) = {%g, %g, %g, charl};\n', [begs(i):ends(i); c(begs(i):ends(i), 1)'; c(begs(i):ends(i), 2)'; c(begs(i):ends(i), 3)']);
+   if smoothtrace
+      fprintf(fid, 'CatmullRom(%g) = {%g:%g};\n', [edgs(1:2, i)'; [begs(i) mids(i); mids(i)-1 ends(i)]]);
+      fprintf(fid, 'CatmullRom(%g) = {%g,%g};\n', [edgs(3:4, i)'; [mids(i)-1 ends(i); mids(i) begs(i)]]);
+      fprintf(fid, 'Line Loop(%g) = {%g, %g, %g, %g};\n', [i; edgs([1, 3, 2, 4], i)]);
+      fprintf(fid, 'Ruled Surface(%g) = {%g};\n', [i, i]);
+
+%   else
+%      ci = [1:(2*nc), 1];
+%      % Standard lines
+%      for j = 1:(2*nc)
+%         fprintf(fid, 'Line(%g) = {%g, %g};\n', j, ci(j), ci(j+1));
+%      end
+%      % Panel separations
+%      for k = 2:(nc-1)
+%         fprintf(fid, 'Line(%g) = {%g, %g};\n', j+k-1, 2*nc-k+1, k);
+%      end
+%      % Line loops and surfaces
+%      % First
+%      fprintf(fid, 'Curve Loop(1) = {%g, %g, %g, %g};\nPlane Surface(1) = {1};\n', 1, -(2*nc+1), 2*nc-1, 2*nc);
+%      % Middle
+%      for l = 2:(nc-2)
+%         fprintf(fid, 'Curve Loop(%g) = {%g, %g, %g, %g};\nPlane Surface(%g) = {%g};\n', l, l, -(2*nc+l), 2*nc-l, 2*nc+l-1, l, l);
+%      end
+%      % Last
+%      fprintf(fid, 'Curve Loop(%g) = {%g, %g, %g, %g};\nPlane Surface(%g) = {%g};\n', nc-1, nc-1, nc, nc+1, 2*nc+(nc-2), nc-1, nc-1);
+   end
+end
+fclose(fid);
+   % Mesh using Gmsh
+   
+% Check for preferences file
+if exist('gmshfaultspref.mat', 'file') ~= 0 % If this .mat file exists, 
+   load('gmshfaultspref.mat', 'gmshpath') % Load it
+else % If not, 
+   if ismac
+      if exist('/Applications/Gmsh.app/Contents/MacOS/gmsh', 'file') % Check for default install location
+         gmshpath = '/Applications/Gmsh.app/Contents/MacOS/';
+      else
+         gmshpath = ''; % Or ask for install location
+         while ~exist([gmshpath filesep 'gmsh'], 'file')
+            gmshpath = input('Enter path to Gmsh application: ');
+         end
+      end
+      % Save Gmsh path to preferences file, to be read in future runs
+      gmfp = fileparts(which('gmshfaults'));
+      save([gmfp filesep 'gmshfaultspref.mat'], 'gmshpath');
+   elseif ispc || (isunix && ~ismac)
+      gmshpath = ''; % Or ask for install location
+      while ~exist([gmshpath filesep 'gmsh.exe'], 'file')
+         gmshpath = input('Enter path to Gmsh application: ');
+      end
+      % Save Gmsh path to preferences file, to be read in future runs
+      gmfp = fileparts(which('gmshfaults'));
+      save([gmfp filesep 'gmshfaultspref.mat'], 'gmshpath');
+   end
+end
+% Do the meshing
+system(sprintf('%s/gmsh -2 faults.geo -o faults.msh -v 0 > junk', gmshpath));
+
+% Remove report file
+if isunix
+  system('rm junk');
+elseif ispc
+  system('del junk');
+end
+  
+% Read in .msh file
+p = ReadPatches('faults.msh');
+  
+%% Make a space-separated string of all .msh files
+%alldir = dir([botdir '/*.msh']);
+%% Sort files numerically
+%alldir = sortnumfilenames(alldir);
+%
+%allstring = '';
+%for i = 1:length(alldir)
+%   allstring = sprintf('%s %s/%s', allstring, alldir(i).folder, alldir(i).name);
+%end
+%allstring = allstring(2:end); % Remove leading space
